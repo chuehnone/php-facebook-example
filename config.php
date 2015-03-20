fd28706e3cccefc06437e31c225658fc65d9de2c
chuehnone <chuehnone@gmail.com>
2015-03-20 14:44:47 +0800

    add readme file

--- config.php
+++ config.php
@@ -5,7 +5,9 @@ use Facebook\FacebookSession;
 
 session_start();
 
-FacebookSession::setDefaultApplication('YOUR_APP_ID', 'YOUR_APP_SECRET');
-
+$appId = 'YOUR_APP_ID';
+$appSecret = 'YOUR_APP_SECRET';
 $redirectUrl = 'http://your.redirect.url/callback.php';
+
+FacebookSession::setDefaultApplication($appId, $appSecret);
 ?>
---
+++ readme.md
@@ -0,0 +1,21 @@
+No Framework to use PHP Facebook Library
+===
+
+This is an easy example
+---
+
+### How to use
+
+#### Required
+1. Facebook permission: 
+> public_profile
+
+> user_friends 
+
+> user_groups 
+
+> read_stream 
+
+#### Set config
+1. set your app key and secret
+2. set your redirect url 
